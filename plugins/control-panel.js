1bcf720ffe0d18e139562513fe79aebb7f1f8457
Aidan McQuay <math0ne@gmail.com>
2017-07-11 04:05:28 -0500

    getting close to usable

---
+++ plugins/control-panel.js
@@ -0,0 +1,24 @@
+const finder  = require('findit')(process.argv[2] || 'plugins/control-panel');
+
+const path = require('path')
+
+
+var Datastore = require('nedb');
+global.db = new Datastore({ filename: './local.json', autoload: true });
+// You can issue commands right away
+
+finder.on('file', function (file, stat) {
+  if(file.slice( -3 ) == "lnk"){
+    //console.log(file);
+    var doc = { file: file,
+                today: new Date(),
+                plugin: "ctrl"
+               };
+
+    db.update( {file: file}, doc, {upsert: true} , function (err, newDoc) {   // Callback is optional
+      //console.log(err);
+      //console.log(newDoc);
+    });
+  }
+
+});
